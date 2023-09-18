651ef84d6a587fd22661d3c1f95167446cc9057a
Bruce Liu <liuxb904@gmail.com>
2023-09-18 16:32:44 -0400

    Added multiset class

---
+++ src/MultiSet.java
@@ -0,0 +1,34 @@
+import java.lang.UnsupportedOperationException;
+class MultiSet {
+    /*
+    An abstract class representing the MultiSet ADT, which supports the
+    add, remove, is_empty, count, and contains operations.
+
+    This class itself does not handle how the underlying data is stored,
+    so it just inherits Object.__init__.
+    */
+
+    public void add(Object item) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(Object item) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean contains(Object item) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int count(Object item) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+}
